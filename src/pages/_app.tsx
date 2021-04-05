346470d374f459096b47ce4c0e2fc4357dd66d4d
Igor Amantino <justin.amantino@gmail.com>
2021-04-05 10:29:05 -0300

    criando theme global

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import { AppProps} from 'next/app'
+import {ChakraProvider} from '@chakra-ui/react'
+import { theme } from '../styles/theme'
 
-function MyApp({ Component, pageProps }:AppProps) { 
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }:AppProps) {
+  return (
+    //ResetCss vem por padrão como true = todo css resetado
+  <ChakraProvider theme={theme}>
+    <Component {...pageProps} />
+  </ChakraProvider>
+ )
 }
 
 export default MyApp
