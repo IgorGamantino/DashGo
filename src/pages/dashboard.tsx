9ccad70abfab9c4e2d89eb8125fadb6ab6cb8a32
Igor Amantino <justin.amantino@gmail.com>
2021-04-05 15:22:02 -0300

    criando e finalizando sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,20 @@
 
+import { Flex } from '@chakra-ui/layout'
 import {Header} from '../components/form/Header'
+import { SideBar } from '../components/form/SideBar'
 
 export default function Dashboard() {
   return (
-   <Header />
+    <Flex
+     direction='column'
+     h='100vh'
+    >
+     <Header />
+
+     <Flex w='100%' my='6' maxW={1480} mx='auto' px='6'>
+        <SideBar />
+     </Flex>
+
+    </Flex>
   )
 }
